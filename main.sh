191f5eb1fe0ebafd127818fd1855074f0d8eb03d
zak39 <baptiste.fotia@arawa.fr>
2022-01-17 12:37:29 +0100

    feat(bash): Split between the main program and the disabling users

--- main.sh
+++ main.sh
@@ -4,17 +4,14 @@ source config.sh
 
 USERS=($(sudo -u $USER_WEB php $PATH_NEXTCLOUD/occ user:list | awk -F "-" '{ gsub(/ /,""); print $2}' | awk -F ":" '{print $1}'))
 
-USERS_DISABLED=()
-USERS_ENABLED=()
+USERS_HAS_LOGGED=()
 
 # Sort users enabled and disabled
 for USER in ${USERS[@]}; do
     lastLogin=$(sudo -u $USER_WEB php $PATH_NEXTCLOUD/occ user:lastseen $USER)
-    if [ "$lastLogin" = "User $USER has never logged in, yet." ]
+    if [ "$lastLogin" != "User $USER has never logged in, yet." ]
     then
-        USERS_DISABLED+=($USER)
-    else
-        USERS_ENABLED+=($USER)
+        USERS_HAS_LOGGED+=($USER)
     fi
 done
 
@@ -22,7 +19,7 @@ done
 # Example: [ foo ] => 1628773604
 declare -A USERS_WITH_DATE
 
-for USER in ${USERS_ENABLED[@]}; do
+for USER in ${USERS_HAS_LOGGED[@]}; do
     DATE_LOGIN=$(sudo -u $USER_WEB php $PATH_NEXTCLOUD/occ user:lastseen $USER | awk '{print $4}')
     YYYY=$(echo $DATE_LOGIN | cut -d '.' -f 3)
     MM=$(echo $DATE_LOGIN | cut -d '.' -f 2)
@@ -30,15 +27,14 @@ for USER in ${USERS_ENABLED[@]}; do
     USERS_WITH_DATE[$USER]=$YYYY-$MM-$DD
 done
 
-# Disabled users who aren't log in from than 3 month (30 days).
-declare -A USERS_TO_DISABLE
+# Build an associative array (USERS_ARE_INACTIVES) who are inactives
+declare -A USERS_ARE_INACTIVES
 DATE_LIMIT=$(date -d "-$NUMBER_DAYS_TO_CHECK days" +%s)
 for USER in "${!USERS_WITH_DATE[@]}"; do
     FORMAT_DATE=$(date -d ${USERS_WITH_DATE[$USER]} +%s)
     if [[ $DATE_LIMIT -gt $FORMAT_DATE ]]; then
-        sudo -u $USER_WEB php $PATH_NEXTCLOUD/occ user:disable $USER
-        echo "$USER is locked"
-        USERS_TO_DISABLE[$USER]=${USERS_WITH_DATE[$USER]}
+        echo "$USER is inactive from ${USERS_WITH_DATE[$USER]}"
+        USERS_ARE_INACTIVES[$USER]=${USERS_WITH_DATE[$USER]}
     fi
 done
 
@@ -50,13 +46,17 @@ if [[ ! -d $_DIR/reports ]]; then
 fi
 
 TODAY=$(date "+%Y%m%d.%H%M")
-if [[ ! -f $_DIR/reports/users_disabled-$TODAY.csv ]]; then
-    touch $_DIR/reports/users_disabled-$TODAY.csv
+if [[ ! -f $_DIR/reports/users_inactive-$TODAY.csv ]]; then
+    touch $_DIR/reports/users_inactive-$TODAY.csv
 fi
 
 
-echo "uid,last-login-date" >> $_DIR/reports/users_disabled-$TODAY.csv
+echo "uid,last-login-date" >> $_DIR/reports/users_inactive-$TODAY.csv
 
-for USER in "${!USERS_TO_DISABLE[@]}"; do
-    echo "$USER,${USERS_TO_DISABLE[$USER]}" >> $_DIR/reports/users_disabled-$TODAY.csv
+for USER in "${!USERS_ARE_INACTIVES[@]}"; do
+    echo "$USER,${USERS_ARE_INACTIVES[$USER]}" >> $_DIR/reports/users_inactive-$TODAY.csv
 done
+
+echo ""
+echo "It's done !"
+echo "Your csv file of users innactives is here : $_DIR/reports/users_inactive-$TODAY.csv ."
